c9e24cce1061bd26c56e17693f125fca967b5f29
Veronika Gordynets <veronika.gordynets@gmail.com>
2020-09-17 16:19:04 +0300

    added solution task for Clean code module

---
+++ cleancode/aircompany/src/main/java/Airport.java
@@ -0,0 +1,132 @@
+import models.MilitaryType;
+import planes.ExperimentalPlane;
+import planes.MilitaryPlane;
+import planes.PassengerPlane;
+import planes.Plane;
+
+import java.util.*;
+
+/**
+ * @author Vitali Shulha
+ * @version 1.1
+ * @date 04/01/2019 - 5:46 PM
+ */
+public class Airport {
+
+    private List<? extends Plane> planes;
+
+    public Airport(List<? extends Plane> planes) {
+        this.planes = planes;
+    }
+
+    public List<? extends Plane> getPlanes() {
+        return planes;
+    }
+
+    public List<PassengerPlane> createPassengerPlanes() {
+        List<PassengerPlane> passengerPlanes = new ArrayList<>();
+        for (Plane plane : planes) {
+            if (plane instanceof PassengerPlane) {
+                passengerPlanes.add((PassengerPlane) plane);
+            }
+        }
+        return passengerPlanes;
+    }
+
+    public List<MilitaryPlane> createMilitaryPlanes() {
+        List<MilitaryPlane> militaryPlanes = new ArrayList<>();
+        for (Plane plane : planes) {
+            if (plane instanceof MilitaryPlane) {
+                militaryPlanes.add((MilitaryPlane) plane);
+            }
+        }
+        return militaryPlanes;
+    }
+
+    public PassengerPlane findPassengerPlaneWithMaxPassengersCapacity() {
+        List<PassengerPlane> passengerPlanes = createPassengerPlanes();
+        PassengerPlane planeWithMaxCapacity = passengerPlanes.get(0);
+        for (int i = 0; i < passengerPlanes.size(); i++) {
+            if (passengerPlanes.get(i).getPassengersCapacity() > planeWithMaxCapacity.getPassengersCapacity()) {
+                planeWithMaxCapacity = passengerPlanes.get(i);
+            }
+        }
+        return planeWithMaxCapacity;
+    }
+
+    public List<MilitaryPlane> findTransportMilitaryPlanes() {
+        List<MilitaryPlane> transportMilitaryPlanes = new ArrayList<>();
+        List<MilitaryPlane> militaryPlanes = createMilitaryPlanes();
+        for (int i = 0; i < militaryPlanes.size(); i++) {
+            MilitaryPlane plane = militaryPlanes.get(i);
+            if (plane.getType() == MilitaryType.TRANSPORT) {
+                transportMilitaryPlanes.add(plane);
+            }
+        }
+        return transportMilitaryPlanes;
+    }
+
+    public List<MilitaryPlane> findBomberMilitaryPlanes() {
+        List<MilitaryPlane> bomberMilitaryPlanes = new ArrayList<>();
+        List<MilitaryPlane> militaryPlanes = createMilitaryPlanes();
+        for (int i = 0; i < militaryPlanes.size(); i++) {
+            MilitaryPlane plane = militaryPlanes.get(i);
+            if (plane.getType() == MilitaryType.BOMBER) {
+                bomberMilitaryPlanes.add(plane);
+            }
+        }
+        return bomberMilitaryPlanes;
+    }
+
+    public List<ExperimentalPlane> findExperimentalPlanes() {
+        List<ExperimentalPlane> experimentalPlanes = new ArrayList<>();
+        for (Plane plane : planes) {
+            if (plane instanceof ExperimentalPlane) {
+                experimentalPlanes.add((ExperimentalPlane) plane);
+            }
+        }
+        return experimentalPlanes;
+    }
+
+    public Airport sortByMaxDistance() {
+        Collections.sort(planes, new Comparator<Plane>() {
+            public int compare(Plane o1, Plane o2) {
+                return o1.getMaxFlightDistance() - o2.getMaxFlightDistance();
+            }
+        });
+        return this;
+    }
+
+    public Airport sortByMaxSpeed() {
+        Collections.sort(planes, new Comparator<Plane>() {
+            public int compare(Plane o1, Plane o2) {
+                return o1.getMaxSpeed() - o2.getMaxSpeed();
+            }
+        });
+        return this;
+    }
+
+    public Airport sortByMaxLoadCapacity() {
+        Collections.sort(planes, new Comparator<Plane>() {
+            public int compare(Plane o1, Plane o2) {
+                return o1.getMaxLoadCapacity() - o2.getMaxLoadCapacity();
+            }
+        });
+        return this;
+    }
+
+    private void printListOfPlanes(Collection<? extends Plane> collection) {
+        Iterator<? extends Plane> iterator = collection.iterator();
+        while (iterator.hasNext()) {
+            Plane plane = iterator.next();
+            System.out.println(plane);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Airport{" +
+                "Planes=" + planes.toString() +
+                '}';
+    }
+}
---
+++ cleancode/aircompany/src/main/java/planes/FirefighterPlane.java
@@ -0,0 +1,7 @@
+package planes;
+
+public class FirefighterPlane extends Plane {
+    public FirefighterPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
+        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+    }
+}
